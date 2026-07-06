cf8495aa2553ca947d68684549ee0fff927bb4e2
yera888 <yeraldinet1234@gmail.com>
2026-07-06 18:14:11 -0400

    Added the SI files

---
+++ src/main/java/com/CRUD_API/HxH/HxHApplication.java
@@ -0,0 +1,13 @@
+package com.CRUD_API.HxH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HxHApplication.class, args);
+	}
+
+}
