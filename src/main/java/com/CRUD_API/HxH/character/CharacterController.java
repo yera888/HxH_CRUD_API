d439c3c88478c785eb41e8c3e3d1cbc75f7b9084
yera888 <yeraldinet1234@gmail.com>
2026-07-08 11:28:07 -0400

    Made a few changes so that details.ftlh works

--- src/main/java/com/CRUD_API/HxH/character/CharacterController.java
+++ src/main/java/com/CRUD_API/HxH/character/CharacterController.java
@@ -1,14 +1,16 @@
 package com.CRUD_API.HxH.character;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-    @Controller
-    public class CharacterController {
+@Controller
+public class CharacterController {
     
     private final CharacterService characterService;
 
-    public CharacterController(CharacterService characterService){
+    public CharacterController(CharacterService characterService) {
         this.characterService = characterService;
 
     }
@@ -17,5 +19,15 @@ import org.springframework.web.bind.annotation.GetMapping;
     public String about(){
     return "about";
     }
+
+    @GetMapping("/character/{id}")
+    public String getCharacterById(@PathVariable long id, Model model) {
+        Character character = characterService.getCharacterById(id);
+        model.addAttribute("character", character);
+
+        return "details";
+
+    }
+    
 
 }
