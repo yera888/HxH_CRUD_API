165ba5576a0e88bfd21e263f0b33af17d4bc8094
yera888 <yeraldinet1234@gmail.com>
2026-07-08 10:58:32 -0400

    updated all of the static folder changes

--- src/main/java/com/CRUD_API/HxH/character/CharacterController.java
+++ src/main/java/com/CRUD_API/HxH/character/CharacterController.java
@@ -1,8 +1,21 @@
 package com.CRUD_API.HxH.character;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class CharacterController {
+    @Controller
+    public class CharacterController {
     
+    private final CharacterService characterService;
+
+    public CharacterController(CharacterService characterService){
+        this.characterService = characterService;
+
+    }
+
+    @GetMapping("/about")
+    public String about(){
+    return "about";
+    }
+
 }
